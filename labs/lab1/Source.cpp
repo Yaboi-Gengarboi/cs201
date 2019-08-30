4aae2ed0444e1cf72cff78809fa98737e327650a
Justyn Durnford <jpdurnford@alaska.edu>
2019-08-30 10:28:43 -0800

    lab1

---
+++ labs/lab1/Source.cpp
@@ -0,0 +1,17 @@
+/*
+Program: Hello, world
+Author: Justyn P Durnford
+Date: 8/30/2019
+*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello, world\n";
+	int a = 52;
+	int b = 89;
+	int c = a * b;
+	std::cout << "a * b = " << c << "\n";
+	return 0;
+}
