52984b6327fa9392e5fa7b18f073f38b6d689993
Justyn Durnford <jpdurnford@alaska.edu>
2019-08-30 10:31:49 -0800

    added hw0 folder

---
+++ hw0/Poem/Source.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "The fool doth think he is wise," << std::endl;
+	std::cout << "but the wise man knows himself to be a fool." << std::endl;
+	return 0;
+}
---
+++ hw0/VideoGame/Source.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Vengeance is an idiot's game.";
+	return 0;
+}
