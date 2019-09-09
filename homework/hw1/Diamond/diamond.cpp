0b0fad1a23a37e7e841757cafdd8e00a1a38bbf3
Justyn Durnford <jpdurnford@alaska.edu>
2019-09-09 09:41:38 -0800

    Added comments & started main framework

---
+++ homework/hw1/Diamond/diamond.cpp
@@ -0,0 +1,17 @@
+/*
+diamond.cpp
+Justyn P. Durnford
+Created on 9/6/2019
+Finished on 
+This program takes a positive integer input, n, and prints a diamond
+with a maximum width of n * (n-1).
+*/
+
+#include <iostream>
+using std::cout;
+using std::cin;
+
+int main()
+{
+	return 0;
+}
