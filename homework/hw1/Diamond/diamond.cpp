d70beb620e702e2cac9d669a229e494346181599
Justyn Durnford <jpdurnford@alaska.edu>
2019-09-09 10:14:20 -0800

    Working on printing diamond

--- homework/hw1/Diamond/diamond.cpp
+++ homework/hw1/Diamond/diamond.cpp
@@ -4,14 +4,33 @@ Justyn P. Durnford
 Created on 9/6/2019
 Finished on 
 This program takes a positive integer input, n, and prints a diamond
-with a maximum width of n * (n-1).
+with a maximum width of 2 * n - 1.
 */
 
 #include <iostream>
 using std::cout;
 using std::cin;
+using std::endl;
 
 int main()
 {
+	int n = 0;
+	cout << "Enter a positive integer: ";
+	cin >> n;
+	if (n <= 0)
+	{
+		cout << n << " is not a positive integer. No diamond will be printed.";
+	}
+	else if (n > 0)
+	{
+		for (int i = 0; i < 2 * n - 1; i++)
+		{
+			for (int x = 0; x < i; x++)
+				cout << " ";
+			for (int p = 0; p < i; p++)
+				cout << "#";
+			cout << endl;
+		}
+	}
 	return 0;
 }
