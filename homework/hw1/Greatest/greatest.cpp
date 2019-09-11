4ea42078ca3067986b9a9fedc18d652c188cdc7d
Justyn Durnford <jpdurnford@alaska.edu>
2019-09-11 09:44:11 -0800

    Started Additional program Greatest

---
+++ homework/hw1/Greatest/greatest.cpp
@@ -0,0 +1,19 @@
+/*
+greatest.cpp
+Justyn P. Durnford
+Created on 9/11/2019
+Finished on 
+
+*/
+
+#include <iostream>
+#include <vector>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+
+int main()
+{
+	return 0;
+}
