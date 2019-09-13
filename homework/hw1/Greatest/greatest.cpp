b0e0d8650e7acb945ebabfab8ad73ea0c25b8934
Justyn Durnford <jpdurnford@alaska.edu>
2019-09-13 09:51:53 -0800

    Updated main function for greatest

--- homework/hw1/Greatest/greatest.cpp
+++ homework/hw1/Greatest/greatest.cpp
@@ -3,7 +3,8 @@ greatest.cpp
 Justyn P. Durnford
 Created on 9/11/2019
 Finished on 
-
+Takes integers input by the user until a negative is entered.
+It will then print the largest of the integers.
 */
 
 #include <iostream>
@@ -15,5 +16,17 @@ using std::vector;
 
 int main()
 {
+	vector<int> ints;
+	int f = 0;
+	cout << "This program will take integers you enter and print the greatest one." << endl;
+	while (f >= 0)
+	{
+		cout << "Enter an integer to add it to the list." << endl;
+		cout << "Enter a negative integer to stop and print the largest integer." << endl;
+		cin >> f;
+		if (f >= 0)
+			ints.push_back(f);
+
+	}
 	return 0;
 }
