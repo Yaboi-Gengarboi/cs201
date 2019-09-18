5f97503bcc03ed584d9b91a2c46cd5cd2dae99f4
Justyn Durnford <jpdurnford@alaska.edu>
2019-09-18 09:28:47 -0800

    Started main

---
+++ homework/hw2/Money/money.cpp
@@ -0,0 +1,17 @@
+/*
+money.cpp
+Justyn P. Durnford
+Created on 9/18/2019
+Finished on
+
+*/
+
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+	return 0;
+}
