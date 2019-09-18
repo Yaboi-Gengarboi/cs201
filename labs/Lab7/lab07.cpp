6c65dfcf33f2e6ab74cada066dc12f4846eb1ef6
Justyn Durnford <jpdurnford@alaska.edu>
2019-09-18 10:10:15 -0800

    Moved code around

--- labs/Lab7/lab07.cpp
+++ labs/Lab7/lab07.cpp
@@ -3,17 +3,17 @@ lab07.cpp
 Justyn P. Durnford
 Created on 9/18/2019
 Finished on
+part1 uses the getInt(); and putInt(); methods added from intio to take 3 integers
+from the user and print them in a neat column.
 */
 
 #include <iostream>
 #include "intio.hpp"
 using std::cout;
-using std::cin;
 using std::endl;
 
-int main()
+void part1()
 {
-
 	cout << "Enter an integer: ";
 	int num1 = getInt();
 	cout << "Enter another integer: ";
@@ -27,6 +27,40 @@ int main()
 	cout << endl;
 	putInt(num3, 10);
 	cout << endl;
+}
 
+void part2()
+{
+
+}
+
+void part3()
+{
+
+}
+
+int main()
+{
+	int f = -1;
+	while (f != 0)
+	{
+		cout << "Enter a part of the lab to test." << endl;
+		cout << "Enter 1 to test part 1." << endl;
+		cout << "Enter 2 to test part 2." << endl;
+		cout << "Enter 3 to test part 3." << endl;
+		cout << "Enter 0 to quit." << endl;
+		f = getInt();
+		switch (f)
+		{
+		case 1:
+			part1();
+			break;
+		case 0:
+			break;
+		default:
+			cout << "Invalid input. Try again." << endl;
+			break;
+		}
+	}
 	return 0;
 }
