792a3055bda0c565d867aae6cc389aca1e8ea5aa
Justyn Durnford <jpdurnford@alaska.edu>
2019-09-18 09:18:53 -0800

    Built main

--- homework/hw2/Names/names.cpp
+++ homework/hw2/Names/names.cpp
@@ -11,11 +11,20 @@ Finished on
 #include <string>
 using std::cout;
 using std::cin;
+using std::getline;
 using std::endl;
 using std::vector;
 using std::string;
 
 int main()
 {
+	vector<string> names;
+	for (int i = 0; i < 10; i++)
+	{
+		string name = "";
+		cout << "Enter a name: ";
+		getline(cin, name);
+		names.push_back(name);
+	}
 	return 0;
 }
