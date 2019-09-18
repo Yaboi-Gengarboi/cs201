6eb96e5cfafb1996861287c07d66a001d98446fe
Justyn Durnford <jpdurnford@alaska.edu>
2019-09-18 09:59:12 -0800

    Finished part 1

--- labs/Lab7/lab07.cpp
+++ labs/Lab7/lab07.cpp
@@ -5,7 +5,28 @@ Created on 9/18/2019
 Finished on
 */
 
+#include <iostream>
+#include "intio.hpp"
+using std::cout;
+using std::cin;
+using std::endl;
+
 int main()
 {
+
+	cout << "Enter an integer: ";
+	int num1 = getInt();
+	cout << "Enter another integer: ";
+	int num2 = getInt();
+	cout << "Enter another integer: ";
+	int num3 = getInt();
+
+	putInt(num1, 10);
+	cout << endl;
+	putInt(num2, 10);
+	cout << endl;
+	putInt(num3, 10);
+	cout << endl;
+
 	return 0;
 }
