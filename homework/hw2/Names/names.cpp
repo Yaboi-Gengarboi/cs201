3414192a260a5f41f05094474f7c72abc6df4119
Justyn Durnford <jpdurnford@alaska.edu>
2019-09-18 09:14:24 -0800

    Started main framework

---
+++ homework/hw2/Names/names.cpp
@@ -0,0 +1,21 @@
+/*
+names.cpp
+Justyn P. Durnford
+Created on 9/18/2019
+Finished on
+
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+using std::string;
+
+int main()
+{
+	return 0;
+}
