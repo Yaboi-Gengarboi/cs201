af1bc83901cd83da55a5007d91e964113a0989d0
Justyn Durnford <jpdurnford@alaska.edu>
2019-09-18 09:38:35 -0800

    Finished input/output

--- homework/hw2/Money/money.cpp
+++ homework/hw2/Money/money.cpp
@@ -13,5 +13,27 @@ using std::endl;
 
 int main()
 {
+	int nPennies = 0;
+	int nNickles = 0;
+	int nDimes = 0;
+	int nQuarters = 0;
+	int nHalfDollars = 0;
+	int nDollars = 0;
+
+	cout << "How many pennies do you have? ";
+	cin >> nPennies;
+	cout << "How many nickles do you have? ";
+	cin >> nNickles;
+	cout << "How many dimes do you have? ";
+	cin >> nDimes;
+	cout << "How many quarters do you have? ";
+	cin >> nQuarters;
+	cout << "How many half dollars do you have? ";
+	cin >> nHalfDollars;
+	cout << "how many dollars do you have? ";
+	cin >> nDollars;
+
+
+
 	return 0;
 }
