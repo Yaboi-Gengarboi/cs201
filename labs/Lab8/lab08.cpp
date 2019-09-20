471066c1a95c0fbd19d308908c97b5c4f936c814
Justyn Durnford <jpdurnford@alaska.edu>
2019-09-20 10:01:41 -0800

    Finished part1

--- labs/Lab8/lab08.cpp
+++ labs/Lab8/lab08.cpp
@@ -1,4 +1,29 @@
-int main()
+/*
+lab08.cpp
+Justyn P. Durnford
+Created on 9/20/2019
+Finished on
+
+*/
+
+#include "intio.hpp"
+#include "lab8.hpp"
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int doInput()
 {
-	return 0;
+	int num = getInt();
+	return num;
+}
+
+int compute(int n)
+{
+	n *= 2;
+	n -= 3;
+	n *= 6;
+	n += 5;
+	n /= 3;
+	return n;
 }
