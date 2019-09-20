c615a0a837afb4ea6698a59410c2966bf4d31ba8
Justyn Durnford <jpdurnford@alaska.edu>
2019-09-20 10:17:51 -0800

    Finished lab & part 2

--- labs/Lab8/lab08.cpp
+++ labs/Lab8/lab08.cpp
@@ -16,5 +16,13 @@ int doInput()
 
 int compute(int n)
 {
-	return n*n;
+	if (n <= 0)
+		return 0;
+	int f = 0;
+	for (int i = 1; i < n / 2 + 1; i++)
+	{
+		if (n % i == 0)
+			f++;
+	}
+	return f;
 }
