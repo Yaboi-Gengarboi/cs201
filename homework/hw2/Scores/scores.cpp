dc0fd64649bd52c7b6ccbefbf39aae67658af1bf
Justyn Durnford <jpdurnford@alaska.edu>
2019-09-20 10:30:55 -0800

    Started main framework

--- homework/hw2/Scores/scores.cpp
+++ homework/hw2/Scores/scores.cpp
@@ -5,3 +5,17 @@ Created on 9/20/2019
 Finished on
 
 */
+
+#include <iostream>
+#include <vector>
+#include <string>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+using std::string;
+
+int main()
+{
+	return 0;
+}
