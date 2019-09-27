f227d6569fb0e41ac5baed3db115da5ac99a31a9
Justyn Durnford <jpdurnford@alaska.edu>
2019-09-27 09:22:12 -0800

    Added void collatz

---
+++ homework/hw3/Collatz/collatz.cpp
@@ -0,0 +1,40 @@
+/*
+collatz.cpp
+Justyn P. Durnford
+Created on 9/27/2019
+Finished on
+
+*/
+
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+/*
+This finction takes an integer input k and
+modifies it according to the collatz conjecture,
+that is:
+If k is even, c(k) = 3 * k + 1
+If k is odd, c(k) = k / 2
+*/
+void collatz(int k)
+{
+	cout << k << " ";
+	while (k > 1)
+	{
+		
+		switch (k % 2)
+		{
+			case 0:
+				k /= 2;
+				break;
+			default:
+				k *= 3;
+				k += 1;
+				break;
+		}
+		cout << k << " ";
+	}
+	cout << endl;
+}
