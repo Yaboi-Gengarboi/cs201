ea319fa6c7374a4ae593c4cb762bb3d19d7862eb
Justyn Durnford <jpdurnford@alaska.edu>
2019-09-27 09:40:40 -0800

    Finished

--- homework/hw3/Collatz/collatz.cpp
+++ homework/hw3/Collatz/collatz.cpp
@@ -2,8 +2,9 @@
 collatz.cpp
 Justyn P. Durnford
 Created on 9/27/2019
-Finished on
-
+Finished on 9/27/2019
+Allows the user to enter numbers into the Collatz
+conjecture multiple times.
 */
 
 #include <iostream>
@@ -13,7 +14,7 @@ using std::endl;
 
 /*
 This finction takes an integer input k and
-modifies it according to the collatz conjecture,
+modifies it according to the Collatz conjecture,
 that is:
 If k is even, c(k) = 3 * k + 1
 If k is odd, c(k) = k / 2
@@ -42,11 +43,27 @@ void collatz(int k)
 int main()
 {
 	int k = 1;
-	cout << "Enter a positive non-zero integer to put in the Collatz conjecture: ";
-	cin >> k;
-	if (k < 1)
-		cout << "ERROR: Invalid input. Try again." << endl;
-	else
-		collatz(k);
+	int f = -1;
+	while (f != 0)
+	{
+		cout << "Enter 1 to test the Collatz conjecture." << endl;
+		cout << "Enter 0 to quit." << endl;
+		cin >> f;
+		switch (f)
+		{
+			case 0:
+				break;
+			case 1:
+				cout << "Enter a positive non-zero integer to put in the Collatz conjecture: ";
+				cin >> k;
+				if (k < 1)
+					cout << "ERROR: Invalid input. Try again." << endl;
+				else
+					collatz(k);
+				break;
+			default:
+				cout << "ERROR: Invalid input. Try again." << endl;
+		}
+	}
 	return 0;
 }
