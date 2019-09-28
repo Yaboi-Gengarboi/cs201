5c02954fa8cf42be20772952076d538d9dd63ead
Justyn Durnford <jpdurnford@alaska.edu>
2019-09-28 14:57:45 -0700

    Completed functionality for normal polynomials

--- homework/hw3/Quadratic/quadratic.cpp
+++ homework/hw3/Quadratic/quadratic.cpp
@@ -20,13 +20,15 @@ Calculates the solution to a polynomial
 via the quadratic equation:
 x = (-b +- sqrt(b^2 - 4 * a * c)) / (2 * a)
 */
-void quadratic(int a, int b, int c)
+void quadratic(double a, double b, double c)
 {
-	int d = pow(b, 2) - 4 * a * c;
+	double x1 = 0.0;
+	double x2 = 0.0;
+	double d = pow(b, 2) - 4 * a * c;
 	if (d < 0)
 	{
 		int f = -1;
-		while (f != 0 || f != 1)
+		while (f != 0 && f != 1)
 		{
 			cout << "WARNING: This polynomial has non-real answers." << endl;
 			cout << "Enter 1 to continue using imaginary numbers." << endl;
@@ -47,14 +49,55 @@ void quadratic(int a, int b, int c)
 	else
 	{
 		int f = -1;
-		while (f != 0)
+		while (f != 1 && f != 2)
 		{
 			cout << "Enter 1 to print the answers in decimal form." << endl;
-			cout << "";
+			cout << "Enter 2 to print the answers in fraction form." << endl;
+			cin >> f;
+			switch (f)
+			{
+				case 1:
+					x1 = (-b - sqrt(d)) / (2 * a);
+					x2 = (-b + sqrt(d)) / (2 * a);
+					cout << x1 << " , " << x2 << endl;
+					break;
+				case 2:
+					cout << "(" << -b << " - " << "sqrt(" << d << ")) / (" << 2 * a << ") , ";
+					cout << "(" << -b << " + " << "sqrt(" << d << ")) / (" << 2 * a << ") , " << endl;
+					break;
+				default:
+					cout << "ERROR: Invalid input. Try again." << endl;
+					break;
+			}
 		}
 	}
+}
 
-	double x1 = 0.0;
-	double x2 = 0.0;
+int main()
+{
+	double a = 0;
+	double b = 0;
+	double c = 0;
+	int f = 1;
+	while (f != 0)
+	{
+		f = -1;
+		cout << "Enter a value for a: ";
+		cin >> a;
+		cout << "Enter a value for b: ";
+		cin >> b;
+		cout << "Enter a value for c: ";
+		cin >> c;
+		quadratic(a, b, c);
 
+		while (f < 0)
+		{
+			cout << "Enter 1 to continue." << endl;
+			cout << "Enter 0 to quit." << endl;
+			cin >> f;
+			if (f < 0)
+				cout << "ERROR: Invalid input. Try again." << endl;
+		}
+	}
+	return 0;
 }
