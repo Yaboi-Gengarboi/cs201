624b7d02bbe2f2603bbc0900fa35732c36d6c8aa
Justyn Durnford <jpdurnford@alaska.edu>
2019-10-15 12:41:36 -0700

    Completed inputPrompt (for now) and added decideAction

--- homework/hw4/FIFO-LIFO/fifo-lifo.cpp
+++ homework/hw4/FIFO-LIFO/fifo-lifo.cpp
@@ -9,6 +9,7 @@ Finished on
 #include <string>
 #include <vector>
 #include <iostream>
+#include <sstream>
 
 using std::string;
 using std::vector;
@@ -16,8 +17,9 @@ using std::cin;
 using std::getline;
 using std::cout;
 using std::endl;
+using std::istringstream;
 
-// First In First Out
+//First In First Out
 
 void fifoPush(vector<string>& container, const string& item)
 {
@@ -33,7 +35,7 @@ string fifoPop(vector<string>& container)
 	return str;
 }
 
-// Last In First Out
+//Last In First Out
 
 void lifoPush(vector<string>& container, const string& item)
 {
@@ -59,7 +61,37 @@ void printContainer(const vector<string>& container)
 
 }
 
+/*
+This function prompts the user for input.
+I am attempting to keep the prompt in its
+own functuion so that there will be no
+problems with cin in a loop. We shall see.
+@RETURN int: Value that will be passed to
+the function XXX to determine which of the
+above functions should be used (or to quit).
+*/
 int inputPrompt()
+{
+	string line;
+	getline(cin, line);
+	istringstream instream(line);
+	//-2 is the starting value and should change
+	int key = -2;
+
+	instream >> key;
+	if (!instream || (key < 0 && key > 100)) //100 is a Placeholder
+	{
+		//Indicates error and will require a repeat
+		return -1;
+	}
+	else
+		return key;
+}
+
+/*
+
+*/
+void decideAction(int key)
 {
 
 }
