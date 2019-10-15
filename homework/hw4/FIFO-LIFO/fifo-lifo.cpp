bb716277c91bafe09203858b0f21de1571ffdbd3
Justyn Durnford <jpdurnford@alaska.edu>
2019-10-15 12:21:53 -0700

    Created bodies of required functions

--- homework/hw4/FIFO-LIFO/fifo-lifo.cpp
+++ homework/hw4/FIFO-LIFO/fifo-lifo.cpp
@@ -6,6 +6,59 @@ Finished on
 
 */
 
+#include <string>
+#include <vector>
+#include <iostream>
+
+using std::string;
+using std::vector;
+using std::cin;
+using std::getline;
+using std::cout;
+using std::endl;
+
+// First In First Out
+
+void fifoPush(vector<string>& container, const string& item)
+{
+
+}
+
+string fifoPop(vector<string>& container)
+{
+	string str = " ";
+
+
+
+	return str;
+}
+
+// Last In First Out
+
+void lifoPush(vector<string>& container, const string& item)
+{
+
+}
+
+string lifoPop(vector<string>& container)
+{
+	string str = " ";
+
+
+
+	return str;
+}
+
+bool isContainerEmpty(const vector<string>& container)
+{
+	return false;
+}
+
+void printContainer(const vector<string>& container)
+{
+
+}
+
 int main()
 {
 	return 0;
