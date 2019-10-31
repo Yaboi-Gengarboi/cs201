0bbc1c941b40feb26d751ea51a24b57e09c4b8e4
Justyn Durnford <jpdurnford@alaska.edu>
2019-10-30 19:35:40 -0700

    Added input text fields in fltk-trunc-main.cpp, shifted all existing elements down by 50 units

--- homework/hw5/FLTK-TRUNC/fltk-trunc-main.cpp
+++ homework/hw5/FLTK-TRUNC/fltk-trunc-main.cpp
@@ -5,20 +5,26 @@ Created on 10/30/2019
 Last Updated on 10/30/2019
 */
 
-#include <FL/Fl.h>
-#include <FL/Fl_Widget.h>
-#include <FL/Fl_Window.h>
-#include <FL/Fl_Box.h>
-#include <FL/Fl_Button.h>
-#include <FL/Fl_ask.h>
+#include <FL/Fl.H>
+#include <FL/Fl_Widget.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Box.H>
+#include <FL/Fl_Button.H>
+#include <FL/Fl_Input.H>
+#include <FL/Fl_ask.H>
 
-void window_callback(Fl_Widget* widget, void*)
+void window_callback(Fl_Widget* widget)
 {
 	if (fl_ask("Do you really want to quit?"))
 		((Fl_Window*)widget)->hide();
 }
 
-void trunc_callback(Fl_Button* button, void*)
+void trunc_callback(Fl_Button* button)
+{
+
+}
+
+void quit_callback(Fl_Button* button)
 {
 
 }
@@ -28,9 +34,15 @@ int main()
 	Fl_Window* window = new Fl_Window(400, 400);
 	window->callback(window_callback);
 
-	Fl_Button* truncButton = new Fl_Button(50, 50, 60, 60, "TRUNC");
+	//String input
+	Fl_Input* textInput = new Fl_Input(50, 100, 300, 25);
 
-	Fl_Button* quitButton = new Fl_Button(120, 50, 60, 60, "QUIT");
+	//Int input
+	Fl_Input* truncInput = new Fl_Input(300, 130, 50, 25);
+
+	Fl_Button* truncButton = new Fl_Button(50, 170, 60, 60, "TRUNC");
+
+	Fl_Button* quitButton = new Fl_Button(120, 170, 60, 60, "QUIT");
 
 
 	window->end();
