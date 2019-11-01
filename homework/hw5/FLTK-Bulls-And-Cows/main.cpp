dcdf7082be1aad0c807562cad32cc691ca7eae74
Justyn Durnford <jpdurnford@alaska.edu>
2019-11-01 09:40:59 -0700

    Included fltk libraries

--- homework/hw5/FLTK-Bulls-And-Cows/main.cpp
+++ homework/hw5/FLTK-Bulls-And-Cows/main.cpp
@@ -7,8 +7,20 @@ Main file for FLTK-Bulls-And-Cows
 */
 
 #include "bulls-and-cows.hpp"
+#include <FL/Fl.H>
+#include <FL/Fl_Widget.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Box.H>
+#include <FL/Fl_Button.H>
+#include <FL/Fl_Input.H>
+#include <FL/Fl_ask.h>
+
+using std::vector;
+
+vector<int> nums;
+vector<int> guess;
 
 int main()
 {
-
+	initVectors(nums, guess);
 }
