aaee27b038a5f40be7d2bbfee62e8f333607e7b9
Justyn Durnford <jpdurnford@alaska.edu>
2019-11-02 18:10:13 -0700

    Merged everything into 1 file, completed

--- homework/hw5/FLTK-FIFO-LIFO/fltk-fifo-lifo.cpp
+++ homework/hw5/FLTK-FIFO-LIFO/fltk-fifo-lifo.cpp
@@ -1,71 +1,31 @@
 /*
 fltk-fifo-lifo.cpp
 Justyn P. Durnford
-Created on 10/15/2019
+Created on 11/2/2019
 Last updated on 11/2/2019
-Allows the user to add items into
-a container and then remove them
-with either the FIFO or LIFO
-principles.
 */
 
-#include "fltk-fifo-lifo.hpp"
-#include <string>
 #include <vector>
-#include <iostream>
-#include <sstream>
+#include <string>
 
-using std::string;
+#include <FL/Fl.H>
+#include <FL/Fl_Widget.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Button.H>
+#include <FL/Fl_Input.H>
+#include <FL/Fl_Multiline_Output.H>
+#include <FL/Fl_ask.h>
+
 using std::vector;
-using std::cin;
-using std::getline;
-using std::cout;
-using std::endl;
-using std::istringstream;
+using std::string;
 
-/*
-This function prompts the user for an item.
-I am attempting to keep the prompt in its
-own functuion so that there will be no
-problems with cin in a loop. We shall see.
-@RETURN string str: string that will be
-passed to the vector.
-*/
-string getItem()
-{
-	cout << "Enter an item" << endl;
-	string str;
-	getline(cin, str);
-	return str;
-}
+Fl_Input* input;
+Fl_Multiline_Output* output;
+Fl_Button* modeButton;
+vector<string> items;
+bool isFIFO = true;
 
-/*
-This function prompts the user for an
-integer that will decide which function
-will be used.
-@RETURN int: The entered key if it is
-valid, or -1 if it is not.
-*/
-int getKey()
-{
-	cout << "Enter 1 to add an item to the container" << endl;
-	cout << "Enter 2 to take an item from the container" << endl;
-	cout << "Enter 0 to quit" << endl;
-
-	int key;
-	string str;
-	getline(cin, str);
-	istringstream instream(str);
-	instream >> key;
-
-	if (!instream || key < 0 || key > 2)
-		return -1; //Indicates error
-	return key;
-}
-
-/*
-Returns true if the vector is empty
-*/
+//Returns true if the vector is empty
 bool isContainerEmpty(const vector<string>& container)
 {
 	if (container.size() == 0)
@@ -75,135 +35,81 @@ bool isContainerEmpty(const vector<string>& container)
 	return false;
 }
 
-/*
-Prints the contents of the container
-*/
-void printContainer(const vector<string>& container)
+//Builds a string that contains every item in the vector
+string toString(const vector<string>& container)
 {
+	string out = "";
 	for (string item : container)
-		cout << item << endl;
-}
-
-//First In First Out
-
-/*
-Adds item to the container
-*/
-void fifoPush(vector<string>& container, const string& item)
-{
-	container.push_back(item);
-	cout << item << " added." << endl;
-}
-
-/*
-Removes the first item in the container
-and returns it
-*/
-string fifoPop(vector<string>& container)
-{
-	string str = " ";
-	if (isContainerEmpty(container))
+		out += item + ", ";
+	if (!out.empty())
 	{
-		cout << "The container is empty." << endl;
+		out.pop_back();
+		out.pop_back();
 	}
+	return out;
+}
+
+//Switches from FIFO to LIFO and vise versa
+void switchMode(Fl_Widget* widget)
+{
+	if (isFIFO)
+		modeButton->label("LIFO");
 	else
-	{
-		str = container[0];
-		cout << str << " removed." << endl;
-		container.erase(container.begin());
-	}
-	return str;
+		modeButton->label("FIFO");
+	isFIFO = !isFIFO;
 }
 
-//Last In First Out
-
-/*
-Adds item to the container
-*/
-void lifoPush(vector<string>& container, const string& item)
+//Allows the window to close
+void quit_callback(Fl_Widget* widget)
 {
-	container.push_back(item);
-	cout << item << " added." << endl;
+	if (fl_ask("Do you really want to quit?"))
+		exit(0);
 }
 
-/*
-Removes the last item in the container
-and returns it
-*/
-string lifoPop(vector<string>& container)
+//Adds item to the vector
+void push_callback(Fl_Widget* widget)
 {
-	string str = " ";
-	if (isContainerEmpty(container))
-	{
-		cout << "The container is empty." << endl;
-	}
-	else
-	{
-		str = container[container.size() - 1];
-		cout << str << " removed." << endl;
-		container.pop_back();
-	}
-	return str;
+	items.push_back(input->value());
+	output->value(toString(items).c_str());
 }
 
-/*
-Test for Fifo
-*/
-bool testFifo()
+//Removes item from the vector
+void pop_callback(Fl_Widget* widget)
 {
-	int key = 1;
-	vector<string> container;
-	string item;
-
-	while (key != 0)
+	if (!isContainerEmpty(items))
 	{
-		key = getKey();
-		switch (key)
+		if (isFIFO)
 		{
-		case 0: //Quit
-			break;
-		case 1:
-			item = getItem();
-			fifoPush(container, item);
-			break;
-		case 2:
-			item = fifoPop(container);
-			break;
-		default:
-			cout << "Invalid input. Try again." << endl;
-			break;
+			items.erase(items.begin());
+			output->value(toString(items).c_str());
+		}
+		else
+		{
+			items.pop_back();
+			output->value(toString(items).c_str());
 		}
 	}
-	return true;
 }
 
-/*
-Test for Lifo
-*/
-bool testLifo()
+int main()
 {
-	int key = 1;
-	vector<string> container;
-	string item;
+	Fl_Window* window = new Fl_Window(400, 450);
+	window->callback(quit_callback);
 
-	while (key != 0)
-	{
-		key = getKey();
-		switch (key)
-		{
-		case 0: //Quit
-			break;
-		case 1:
-			item = getItem();
-			lifoPush(container, item);
-			break;
-		case 2:
-			item = lifoPop(container);
-			break;
-		default:
-			cout << "Invalid input. Try again." << endl;
-			break;
-		}
-	}
-	return true;
+	modeButton = new Fl_Button(350, 10, 40, 40);
+	modeButton->label("FIFO");
+	modeButton->callback(switchMode);
+
+	output = new Fl_Multiline_Output(30, 60, 340, 200);
+	input = new Fl_Input(30, 280, 340, 50);
+
+	Fl_Button* pushButton = new Fl_Button(130, 350, 60, 60, "PUSH");
+	pushButton->callback(push_callback);
+
+	Fl_Button* popButton = new Fl_Button(210, 350, 60, 60, "POP");
+	popButton->callback(pop_callback);
+
+	window->end();
+	window->show();
+	return Fl::run();
 }
