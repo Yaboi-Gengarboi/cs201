62b25134288958534b7f25893f7fb09075e753d8
Justyn Durnford <jpdurnford@alaska.edu>
2019-11-06 09:57:29 -0800

    Working on int normalRandomBetween() in random-map.cpp

--- homework/hw6/Random_Map/random-map.cpp
+++ homework/hw6/Random_Map/random-map.cpp
@@ -9,6 +9,7 @@ Last updated on 11/6/2019
 #include <random>
 #include <map>
 #include <string>
+#include <vector>
 #include <iostream>
 
 using std::random_device;
@@ -18,6 +19,8 @@ using std::uniform_int_distribution;
 /*
 Generates a random integer between and including low
 and high.
+We will assume (because we know the user will not be 
+controlling it) that low is less than high.
 @PARAM int low: Lower integer bound for random number
 generation.
 @PARAM int high: Upper integer bound for random number
@@ -38,6 +41,23 @@ int uniformRandomBetween(int low, int high)
 
 	//Assigns a random number to randNum
 	randNum = uniform_dist(engine);
+
+	return randNum;
+}
+
+/*
+
+*/
+int normalRandomBetween(int low, int high)
+{
+	//DUMMY RETURN VALUE
+	int randNum = 0;
+
+	//Mean value
+	int mean = (low + high) / 2;
+
+	//Create random device
+	random_device rDevice;
 
 	return randNum;
 }
