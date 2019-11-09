100a44b4d91ac282036ae0bf4c49861eb0f3e888
Justyn Durnford <jpdurnford@alaska.edu>
2019-11-09 15:55:16 -0800

    Completed hangman.cpp

--- homework/hw6/Hangman/hangman.cpp
+++ homework/hw6/Hangman/hangman.cpp
@@ -3,7 +3,7 @@ hangman.cpp
 Justyn P. Durnford
 Created on 11/9/2019
 Last updated on 11/9/2019
-
+A game of hangman
 */
 
 #include <algorithm>
@@ -12,6 +12,7 @@ Last updated on 11/9/2019
 #include <string>
 #include <random>
 #include <iostream>
+#include <cctype>
 
 using std::cout;
 using std::cin;
@@ -23,6 +24,7 @@ using std::string;
 using std::random_device;
 using std::default_random_engine;
 using std::uniform_int_distribution;
+using std::toupper;
 
 //Prompts the user to enter a letter.
 char getInput()
@@ -37,7 +39,7 @@ char getInput()
 		getline(cin, line);
 		if (!line.empty())
 		{
-			ch = line[0];
+			ch = toupper(line[0]);
 			cont = false;
 		}
 	}
@@ -49,15 +51,17 @@ char getInput()
 Looks if the character from getInput is in
 the word and corrects guess accordingly.
 */
-bool checkGuess(const string& word, string& guess)
+bool checkGuess(const string& word, string& guess, map<char, bool>& letters)
 {
 	bool retVal = false;
 	char ch = getInput();
+	letters[ch] = false;
 	size_t found = word.find(ch);
 
 	//Checks for ch multiple times
 	while (found != -1)
 	{
+		letters[ch] = true;
 		guess[found] = ch;
 		found = word.find(ch, found + 1);
 		retVal = true;
@@ -66,52 +70,104 @@ bool checkGuess(const string& word, string& guess)
 	return retVal;
 }
 
+/*
+Prints the hangman that depends on how
+many guesses the person has gotten
+wrong.
+*/
+void printHangman(int wrong)
+{
+	cout << "---------" << endl;
+	cout << "|       |" << endl;
+	cout << "|       ";
+	if (wrong > 0)
+		cout << "O";
+	cout << endl;
+	cout << "|      "; 
+	if (wrong > 1)
+		cout << "/";
+	if (wrong > 2)
+		cout << "|";
+	if (wrong > 3)
+		cout << "\\";
+	cout << endl;
+	cout << "|      ";
+	if (wrong > 4)
+		cout << "/";
+	if (wrong > 5)
+		cout << " \\";
+	cout << endl;
+}
+
 int main()
 {
 	vector<string> words =
 	{
-		"Apple", "Bagpipes", "Banjo",
-		"Computer", "Crypt", "Danger",
-		"Eaten", "Favorite", "Fishing",
-		"Gaming", "Great", "Haiku",
-		"Happy", "Ivory", "Journal",
-		"Kayak", "Klutz", "Length", 
-		"Mailbox", "Memory", "Nearby", 
-		"Nickel", "Oxygen", "Pajama", 
-		"Pixel", "Polka", "Queen", 
-		"Quick", "Rhythm", "Rogue", 
-		"Sphinx", "Shovle", "Today", 
-		"Twenty", "Unzip", "Violin", 
-		"Western", "Wonder", "Xenon",
-		"Yogurt", "Zigzag", "Zombie"
+		"APPLE", "BAGPIPES", "BANJO",
+		"COMPUTER", "CRYPT", "DANGER",
+		"EATEN", "FAVORITE", "FISHING",
+		"GAMING", "GREAT", "HAIKU",
+		"HAPPY", "IVORY", "JOURNAL",
+		"KAYAK", "KLUTZ", "LENGTH", 
+		"MAILBOX", "MEMORY", "NEARBY", 
+		"NICKEL", "OXYGEN", "PAJAMAS", 
+		"PIXEL", "POLKA", "QUEEN", 
+		"QUICK", "RHYTHM", "ROGUE", 
+		"SPHINX", "SHOVEL", "TODAY", 
+		"TWENTY", "UNZIP", "VIOLIN", 
+		"WESTERN", "WONDER", "XENON",
+		"YOGURT", "ZIGZAG", "ZOMBIE"
 	};
 
 	string word = "";
 	string guess = "";
+	int wrong = 0;
+
+	map <char, bool> letters;
 
 	random_device rDev;
 	default_random_engine engine(rDev());
 	uniform_int_distribution<int> uniform_dist(0, words.size() - 1);
 
 	word = words[uniform_dist(engine)];
-	for (int i = 0; i < word.size(); ++i)
+	for (size_t i = 0; i < word.size(); ++i)
 		guess += "_";
 
-	cout << word << endl;
-	cout << guess << endl;
-	while (guess.compare(word) != 0)
+	printHangman(0);
+	cout << guess << "(" << guess.size() << ") letters" << endl;
+
+	while (guess.compare(word) != 0 && wrong < 6)
 	{
-		if (checkGuess(word, guess))
+		if (checkGuess(word, guess, letters))
 		{
-			cout << guess << endl;
+			printHangman(wrong);
+			cout << guess << "(" << guess.size() << ") letters" << endl;
+			cout << "Wrong letters used: ";
+			for (const auto& letter : letters)
+			{
+				if (!letter.second)
+					cout << letter.first;
+			}
+			cout << endl;
 		}
 		else
 		{
-			cout << guess << endl;
+			++wrong;
+			printHangman(wrong);
+			cout << guess << "(" << guess.size() << ") letters" << endl;
+			cout << "Wrong letters used: ";
+			for (const auto& letter : letters)
+			{
+				if (!letter.second)
+					cout << letter.first;
+			}
+			cout << endl;
+			if (wrong == 6)
+				cout << "Game Over!" << endl;
 		}
 	}
 
-	cout << "You Win!" << endl;
+	cout << "The word was " << word << endl;
 
 	return 0;
 }
