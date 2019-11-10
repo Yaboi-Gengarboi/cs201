2d3f4fee1a0a367fbf1bc02f3e21b94f8c1a9552
Justyn Durnford <jpdurnford@alaska.edu>
2019-11-10 13:54:08 -0800

    Fixed some issues in printCart in shopping.cpp

--- homework/hw6/Shopping/shopping.cpp
+++ homework/hw6/Shopping/shopping.cpp
@@ -10,14 +10,23 @@ Last updated on 11/0/2019
 #include <map>
 #include <string>
 #include <vector>
+#include <stdexcept> 
 
 using std::cin;
 using std::cout;
 using std::endl;
 using std::getline;
+using std::stoi;
+using std::to_string;
 using std::map;
 using std::string;
 using std::vector;
+
+vector<string> items =
+{
+	"Apple", "Banana", "Orange",
+	"Strawberry", "Watermelon"
+};
 
 //Record structure
 struct Record
@@ -48,19 +57,23 @@ void printVector(const vector<string>& vec)
 //Prints items in the cart and total
 void printCart(const map<string, Record>& cart)
 {
+	string test = "";
+
 	string out = "";
+	int amount = 0;
+	double price = 0.0;
 	double total = 0.0;
 
 	for (const auto& item : cart)
 	{
-		total += item.second.price;
+		total += item.second.price * item.second.units;
 		out += item.first;
 		out += ": ";
-		out += item.second.units;
+		out += to_string(item.second.units);
 		out += ", ";
 	}
 	out += "Total : $";
-	out += total;
+	out += to_string(total);
 
 	cout << out << endl;
 }
@@ -73,19 +86,63 @@ string getInput()
 	return input;
 }
 
+//Decides what to do from user input. Returns false to quit.
+bool decideAction(map<string, Record>& cart)
+{
+	string in = getInput();
+	int amount = 0;
+
+	if (in == "Quit") return false;
+	if (in == "Print Cart")
+	{
+		printCart(cart);
+		return true;
+	}
+	if (in == "Print Items")
+	{
+		printVector(items);
+		return true;
+	}
+	for (const string& item : items)
+	{
+		if (in == item)
+		{
+			cout << "How many?" << endl;
+			in = getInput();
+
+			try
+			{
+				amount = stoi(in, nullptr);
+				cart[item].units += amount;
+				cout << amount << " " << item << "(s) added." << endl;
+				return true;
+			}
+			catch (const std::invalid_argument & ia)
+			{
+				cout << "Invalid input. Try again." << endl;
+				return true;
+			}
+		}
+	}
+
+	cout << "Invalid input. Try again." << endl;
+
+	return true;
+}
+
 int main()
 {
 	bool cont = true;
 	map<string, Record> cart;
-	vector<string> items =
-	{
-		"Apples", "Bananas", "Oranges",
-		"Strawberries", "Watermelon"
-	};
 
 	while (cont)
 	{
-
+		cout << "Enter an item to add it to the cart." << endl;
+		cout << "Enter \"Print Items\" to see the available items." << endl;
+		cout << "Enter \"Print Cart\" to see your cart." << endl;
+		cout << "Enter \"Quit\" to quit." << endl;
+		cont = decideAction(cart);
+		cout << endl;
 	} 
 
 	return 0;
