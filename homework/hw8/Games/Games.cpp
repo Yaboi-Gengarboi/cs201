24ee23194a7d50b6094dae3032d5e837e2bf5181
Justyn Durnford <jpdurnford@alaska.edu>
2019-12-04 09:54:25 -0800

    ---Games.cpp--- -Modified hasPlayerWon() to return an int corresponding to the winning player instead of a boolean

--- homework/hw8/Games/Games.cpp
+++ homework/hw8/Games/Games.cpp
@@ -229,28 +229,53 @@ void printBoard(const const array<array<char, 3>, 3>& board)
 }
 
 //Determins if a player has won
-//Returns true if a player has won
-//Returns false otherwise
-bool hasPlayerWon(const array<array<char, 3>, 3>& board)
+//Returns 1 if player 1 (X) has won
+//Returns 2 if player 2 (O) has won
+//Returns 0 if no player has won
+int hasPlayerWon(const array<array<char, 3>, 3>& board)
 {
 	for (int i = 0; i < 3; ++i)
 	{
 		//Checks rows
-		if (board[i][0] == board[i][1] == board[i][2] && board[i][0] != ' ')
-			return true;
+		if (board[i][0] == board[i][1] &&
+			board[i][0] == board[i][2] &&
+			board[i][0] == 'X')
+			return 1;
+		else if (board[i][0] == board[i][1] &&
+				 board[i][0] == board[i][2] && 
+				 board[i][0] == 'O')
+			return 2;
 		//Checks columns
-		else if (board[0][i] == board[0][i] == board[0][i] && board[0][i] != ' ')
-			return true;
+		else if (board[0][i] == board[1][i] &&
+				 board[0][i] == board[2][i] &&
+				 board[0][i] == 'X')
+			return 1;
+		else if (board[0][i] == board[1][i] &&
+				 board[0][i] == board[2][i] &&
+				 board[0][i] == 'O')
+			return 2;
 	}
 
 	//Checks down-right diagonal
-	if (board[0][0] == board[1][1] == board[2][2] && board[0][0] != ' ')
-		return true;
+	if (board[0][0] == board[1][1] &&
+		board[0][0] == board[2][2] &&
+		board[0][0] == 'X')
+		return 1;
+	else if (board[0][0] == board[1][1] &&
+			 board[0][0] == board[2][2] &&
+			 board[0][0] == 'O')
+		return 2;
 	//Checks down-left diagonal
-	else if (board[0][2] == board[1][1] == board[2][0] && board[0][0] != ' ')
-		return true;
+	else if (board[0][2] == board[1][1] &&
+			 board[0][2] == board[2][0] &&
+			 board[0][2] == 'X')
+		return 1;
+	else if (board[0][2] == board[1][1] &&
+			 board[0][2] == board[2][0] &&
+			 board[0][2] == 'O')
+		return 2;
 
-	return false;
+	return 0;
 }
 
 void playTicTacToe()
@@ -336,9 +361,11 @@ int main()
 	}
 	*/
 
-	array<array<char, 3>, 3> board = { { { 'X', 'X', 'X' }, { 'X', 'X', 'X' }, { 'X', 'X', 'X' } } };
+	array<array<char, 3>, 3> board = { { { 'O', ' ', ' ' }, { ' ', 'O', ' ' }, { ' ', ' ', 'O' } } };
 
 	printBoard(board);
+	cout << isBoardFull(board) << endl;
+	cout << hasPlayerWon(board) << endl;
 
 	return 0;
 }
